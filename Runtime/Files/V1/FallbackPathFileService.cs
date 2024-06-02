01e78429e586b4d98a9a97838e3766bcbf72cd4f
ETdoFresh <ETdoFresh@gmail.com>
2024-06-02 11:38:49 -0500

    Update HTMLUGUIView and HTMLUGUIController for improved layout rendering
    The HTMLUGUIView.cs script now references the index.html file using a relative path, "html/index.html", enhancing portability. In the HTMLUGUIController.cs, the Render method has been refactored to optimize the rendering process. It now calculates and sets the positions of individual elements, ensuring they fit within the parent container and wrap when necessary. The updated algorithm significantly improves the visual consistency of HTML-based UIs in Unity.
    
    Additionally, the FallbackPathFileService.cs in the SharedServices.Files package has been modified to support finding, copying, or creating files from various sources, such as the persistent data path, addressable assets, and resources. This change ensures a more reliable file handling process when working with different project configurations.

--- Runtime/Files/V1/FallbackPathFileService.cs
+++ Runtime/Files/V1/FallbackPathFileService.cs
@@ -27,13 +27,14 @@ namespace SharedServices.Files.V1
 
         public FileMonitor StartMonitoring(string path)
         {
+            FindCopyOrCreateFile(path);
             var persistentDataPath = Application.persistentDataPath;
             path = Path.Combine(persistentDataPath, path);
             var fileMonitor = new FileMonitor(path);
             _fileMonitorMonoBehaviour.AddFileMonitor(fileMonitor);
             return fileMonitor;
         }
-
+        
         public void StopMonitoring(FileMonitor fileMonitor)
         {
             _fileMonitorMonoBehaviour.RemoveFileMonitor(fileMonitor);
@@ -51,6 +52,14 @@ namespace SharedServices.Files.V1
             path = Path.Combine(persistentDataPath, path);
             var json = JsonUtil.ToJson(obj);
             WriteAllText(path, json);
+        }
+        
+        private void FindCopyOrCreateFile(string path)
+        {
+            if (ExistsInPersistentDataPath(path)) return;
+            // else if (ExistsInAddressable(path)) CopyFromAddressableToPersistentDataPath(path);
+            else if (ExistsInResources(path)) CopyFromResourcesToPersistentDataPath(path);
+            else CreateEmptyFileInPersistentDataPath(path);
         }
 
         private string ReadTextFromFallbackSources(string path)
@@ -80,6 +89,60 @@ namespace SharedServices.Files.V1
             }
             
             return null;
+        }
+        
+        private bool ExistsInPersistentDataPath(string path)
+        {
+            var persistentDataPath = Application.persistentDataPath;
+            var fullPath = Path.Combine(persistentDataPath, path);
+            return File.Exists(fullPath);
+        }
+        
+        private bool ExistsInAddressable(string path) => throw new System.NotImplementedException();
+
+        private bool ExistsInResources(string path) => GetTextAssetResource(path);
+        
+        private bool IsAddressableInstalled() => throw new System.NotImplementedException();
+        private bool AddressableExists(string path) => throw new System.NotImplementedException();
+        private string ReadTextFromAddressable(string path) => throw new System.NotImplementedException();
+        
+        private TextAsset GetTextAssetResource(string path)
+        {
+            var textResource = Resources.Load<TextAsset>(path);
+            if (textResource)
+            {
+                return textResource;
+            }
+            
+            var pathWithoutExt = Path.ChangeExtension(path, null);
+            textResource = Resources.Load<TextAsset>(pathWithoutExt);
+            return textResource;
+        }
+        
+        private void CopyFromResourcesToPersistentDataPath(string path)
+        {
+            CreateDirectoriesIfNotExists(path);
+            var textResource = GetTextAssetResource(path);
+            var persistentDataPath = Application.persistentDataPath;
+            var fullPath = Path.Combine(persistentDataPath, path);
+            File.WriteAllText(fullPath, textResource.text);
+        }
+
+        private void CreateEmptyFileInPersistentDataPath(string path)
+        {
+            var persistentDataPath = Application.persistentDataPath;
+            var fullPath = Path.Combine(persistentDataPath, path);
+            File.WriteAllText(fullPath, string.Empty);
+        }
+
+        private void CreateDirectoriesIfNotExists(string path)
+        {
+            var persistentDataPath = Application.persistentDataPath;
+            var directory = Path.GetDirectoryName(path);
+            if (string.IsNullOrEmpty(directory)) return;
+            var fullPath = Path.Combine(persistentDataPath, directory);
+            if (!Directory.Exists(fullPath)) 
+                Directory.CreateDirectory(fullPath);
         }
     }
 }
